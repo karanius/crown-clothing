371d426e4e3b3cc07bedb651ec4bd38958dd8484
Kavian Darvish <kavian1991@gmail.com>
2019-12-14 19:33:35 -0500

    broken out homepage into smaller component , finished styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 
-import HomePage from './homepage.component.jsx'
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
