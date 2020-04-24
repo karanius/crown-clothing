a80ba655a7be1d290b65f57eaebb8510338b0f0d
Kavian Darvish <kavian1991@gmail.com>
2020-04-24 13:45:28 -0400

    configuring procees.env

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,14 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [];
+console.log(process.env)
+
+if (process.env.NODE_ENV === 'development'){
+  middleWares.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export default store;
+
