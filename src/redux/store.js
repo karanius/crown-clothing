f33df51f7b433ffbea3033f0d687c7b7949b9c7b
Kavian Darvish <kavian1991@gmail.com>
2020-04-25 11:35:26 -0400

    updating applicatin with reselect, replaced cart selectors with better ones

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,6 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middleWares = [];
-console.log(process.env)
 
 if (process.env.NODE_ENV === 'development'){
   middleWares.push(logger)
